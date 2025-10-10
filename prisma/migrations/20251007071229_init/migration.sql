ece40982ea814c7f30cc8e29be39454fec6e374b
Nadir Septian Hadiansyah <nadirhexonyte@gmail.com>
2025-10-10 16:18:06 +0700

    Delete prisma directory

--- prisma/migrations/20251007071229_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `logged_in` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."User" DROP COLUMN "logged_in";
